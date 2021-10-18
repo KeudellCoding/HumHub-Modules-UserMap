b43bdd8c7bd4bd86d6f6bf00dd10896296592b15
KarlKeu00 <47644344+KarlKeu00@users.noreply.github.com>
2021-10-18 21:31:57 +0200

    Adding icon (Thanks to HumHub for the design)

--- Module.php
+++ Module.php
@@ -8,11 +8,14 @@
 
 namespace humhub\modules\usermap;
 
-use Yii;
 use yii\helpers\Url;
-use yii\web\JsonParser;
 
 class Module extends \humhub\components\Module {
+    /**
+     * @inheritdoc
+     */
+    public $resourcesPath = 'resources';
+
     /**
      * @inheritdoc
      */
